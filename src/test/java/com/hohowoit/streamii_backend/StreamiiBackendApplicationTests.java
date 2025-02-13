2dd65277440ac39c2dd7559b21500a6777ee9df5
hohowoit <thqudcks114@naver.com>
2025-02-13 16:25:49 +0900

    새로운 초기 파일 추가

---
+++ src/test/java/com/hohowoit/streamii_backend/StreamiiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hohowoit.streamii_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamiiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
